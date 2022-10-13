5e81c664160a9171b1a8c376a6a3c5a565f1e61b
VladyslavKozlov2101git <vladislavkozlov2101git@gmail.com>
2022-10-13 16:10:30 +0300

    React Testing Tutorial - 9 - Test Driven Development

---
+++ src/components/Greet.test.tsx
@@ -0,0 +1,17 @@
+
+import { render, screen } from '@testing-library/react'
+import { Greet } from './Greet'
+
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />)
+    const textElement = screen.getByText('Hello Guest')
+    expect(textElement).toBeInTheDocument()
+  })
+
+  test('renders a name', () => {
+    render(<Greet name="Vladyslav" />)
+    const textElement = screen.getByText('Hello Vladyslav')
+    expect(textElement).toBeInTheDocument()
+  })
+})
